0151f4f18ac8fb18ec2e9b2968608c1f7fd3c230
KEVIN <karimboyevshaxram97@gmail.com>
2026-05-03 09:14:58 +0900

    feat: SASS integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,19 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Container>
-      <Stack flexDirection={"column"}>
-        <Box>Popular Properties</Box>
-        <Box>Top Agents</Box>
-        <Box>Top Properties</Box>
-        <Box>Events</Box>
-      </Stack>
-    </Container>
+    <Stack>
+  <Stack flexDirection={"column"}>
+    <Stack>
+      <Stack className="container">Popular Properties</Stack>
+    </Stack>
+    <Stack>
+      <Stack className="container">Top Agents</Stack>
+    </Stack>
+    <Stack>
+      <Stack className="container">Top Properties</Stack>
+    </Stack>
+  </Stack>
+</Stack>
   );
 };
 

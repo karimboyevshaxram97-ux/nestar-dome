7a84cdc0d7bb4b50fbd69ff1a32a42afe6ebbdb6
KEVIN <karimboyevshaxram97@gmail.com>
2026-05-05 17:41:50 +0900

    feat: property  publishing done

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -1,8 +1,12 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
 
-const PropertyDetail = () => {
-  return <Container>PROPERTY DETAIL</Container>;
+const PropertyDetail: NextPage = () => {
+
+
+    return <Container>PROPERTY DETAIL</Container>;
+  
 };
 
 export default withLayoutBasic(PropertyDetail);
